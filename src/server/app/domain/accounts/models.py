a3cf9ee75c8d19e1e637bdf121121f4b25cce5b4
Cody Fincher <cody.fincher@gmail.com>
2023-01-21 11:05:12 -0600

    Implemented migrations

--- src/server/app/domain/accounts/models.py
+++ src/server/app/domain/accounts/models.py
@@ -2,7 +2,7 @@ from datetime import date
 from typing import Annotated
 
 import sqlalchemy as sa
-from pydantic import EmailStr
+from pydantic import constr
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.lib import dto
@@ -11,19 +11,31 @@ from app.lib.db import orm
 __all__ = ["User"]
 
 
+def check_email(email: str) -> str:
+    """Validate an email."""
+    if "@" not in email:
+        raise ValueError("Invalid email!")
+    return email
+
+
 class User(orm.DatabaseModel):
     """User Model."""
 
     __tablename__ = "user_account"  # type: ignore[assignment]
     __table_args__ = {"comment": "User accounts for application access"}
-    email: Mapped[EmailStr] = mapped_column(unique=True, index=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        unique=True,
+        index=True,
+        nullable=False,
+        info=dto.field(validators=[check_email], pydantic_type=constr(to_lower=True)),
+    )
     name: Mapped[str] = mapped_column(nullable=True)
-    hashed_password: Mapped[str] = mapped_column(sa.String(length=255), nullable=True, info={"dto": dto.Mark.PRIVATE})
-    is_active: Mapped[bool] = mapped_column(default=True, nullable=False, info={"dto": dto.Mark.READ_ONLY})
-    is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False, info={"dto": dto.Mark.READ_ONLY})
-    is_verified: Mapped[bool] = mapped_column(default=False, nullable=False, info={"dto": dto.Mark.READ_ONLY})
-    verified_at: Mapped[date] = mapped_column(nullable=True, info={"dto": dto.Mark.READ_ONLY})
-    joined_at: Mapped[date] = mapped_column(default=date.today, info={"dto": dto.Mark.READ_ONLY})
+    hashed_password: Mapped[str] = mapped_column(sa.String(length=255), nullable=True, info=dto.field("private"))
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False, info=dto.field("read-only"))
+    is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False, info=dto.field("read-only"))
+    is_verified: Mapped[bool] = mapped_column(default=False, nullable=False, info=dto.field("read-only"))
+    verified_at: Mapped[date] = mapped_column(nullable=True, info=dto.field("read-only"))
+    joined_at: Mapped[date] = mapped_column(default=date.today, info=dto.field("read-only"))
 
 
 UserReadDTO = dto.FromMapped[Annotated[User, "read"]]
