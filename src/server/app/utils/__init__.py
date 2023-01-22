ca68c70e7eb1f67b098a53a1d31f0920f3d0bed4
Cody Fincher <cody@gluent.com>
2023-01-22 17:35:53 -0600

    feat: add slugify function

--- src/server/app/utils/__init__.py
+++ src/server/app/utils/__init__.py
@@ -3,6 +3,6 @@
 Stores useful libraries, helpers or utilities for various application
 components.
 """
-from . import module_loader
+from . import module_loader, text
 
-__all__ = ["module_loader"]
+__all__ = ["module_loader", "text"]
---
+++ src/server/app/utils/text.py
@@ -0,0 +1,37 @@
+import re
+import unicodedata
+
+__all__ = ["check_email"]
+
+
+def check_email(email: str) -> str:
+    """Validate an email."""
+    if "@" not in email:
+        raise ValueError("Invalid email!")
+    return email
+
+
+def slugify(value: str, allow_unicode: bool = False, separator: str | None = None) -> str:
+    """slugify.
+
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+
+    Args:
+        value (str): the string to slugify
+        allow_unicode (bool, optional): allow unicode characters in slug. Defaults to False.
+        separator (str, optional): by default a `-` is used to delimit word boundaries.  Set this to configure to something different.
+
+    Returns:
+        str: a slugified string of the value parameter
+    """
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    if separator is not None:
+        return re.sub(r"[-\s]+", "-", value).strip("-_").replace("-", separator)
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
