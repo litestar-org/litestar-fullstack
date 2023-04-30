4e00391c99128ce05f7fbc7c73f3f43edc935f8a
Cody Fincher <cody@gluent.com>
2023-04-30 18:13:54 -0500

    chore: linting updates

--- src/app/domain/analytics/schemas.py
+++ src/app/domain/analytics/schemas.py
@@ -4,6 +4,8 @@ from datetime import datetime  # noqa: TCH003
 
 from app.lib.schema import CamelizedBaseModel
 
+__all__ = ["NewUsersByWeek"]
+
 
 class NewUsersByWeek(CamelizedBaseModel):
     """User properties to use for a response."""
