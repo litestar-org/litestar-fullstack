fccd30674eab65b908dba138e1de8c99f576f049
Cody Fincher <codyfincher@gmail.com>
2023-05-07 15:11:38 -0500

    feat: adds tests for crypt functions

--- src/app/lib/crypt.py
+++ src/app/lib/crypt.py
@@ -2,16 +2,13 @@ from __future__ import annotations
 
 import base64
 import logging
-from typing import TYPE_CHECKING
 
 from litestar.utils.sync import AsyncCallable
 from passlib.context import CryptContext
+from pydantic import SecretBytes, SecretStr
 
 __all__ = ["get_encryption_key", "get_password_hash", "verify_password"]
 
-
-if TYPE_CHECKING:
-    from pydantic import SecretBytes, SecretStr
 
 logger = logging.getLogger()
 
@@ -28,11 +25,12 @@ def get_encryption_key(secret: str) -> bytes:
     Returns:
         bytes: a URL safe encoded version of secret
     """
-    padded_secret = f"{secret:<32}"[0:32]
-    return base64.urlsafe_b64encode(padded_secret.encode())
+    if len(secret) <= 32:
+        secret = f"{secret:<32}"[0:32]
+    return base64.urlsafe_b64encode(secret.encode())
 
 
-async def get_password_hash(password: SecretBytes | SecretStr) -> str:
+async def get_password_hash(password: SecretBytes | SecretStr | str | bytes) -> str:
     """Get password hash.
 
     Args:
@@ -40,10 +38,12 @@ async def get_password_hash(password: SecretBytes | SecretStr) -> str:
     Returns:
         str: Hashed password
     """
-    return await AsyncCallable(password_crypt_context.hash)(secret=password.get_secret_value())
+    if isinstance(password, SecretBytes | SecretStr):
+        password = password.get_secret_value()
+    return await AsyncCallable(password_crypt_context.hash)(secret=password)
 
 
-async def verify_password(plain_password: SecretBytes | SecretStr, hashed_password: str) -> bool:
+async def verify_password(plain_password: SecretBytes | SecretStr | str | bytes, hashed_password: str) -> bool:
     """Verify Password.
 
     Args:
@@ -53,8 +53,10 @@ async def verify_password(plain_password: SecretBytes | SecretStr, hashed_passwo
     Returns:
         bool: True if the password hashes match
     """
+    if isinstance(plain_password, SecretBytes | SecretStr):
+        plain_password = plain_password.get_secret_value()
     valid, _ = await AsyncCallable(password_crypt_context.verify_and_update)(
-        secret=plain_password.get_secret_value(),
+        secret=plain_password,
         hash=hashed_password,
     )
     return bool(valid)
