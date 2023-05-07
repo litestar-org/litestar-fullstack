fb666b2921b625522c8bc3bcf5c64cb89df72e27
Cody Fincher <codyfincher@gmail.com>
2023-05-07 15:39:42 -0500

    feat: adds additional tests for user login

--- tests/integration/test_access.py
+++ tests/integration/test_access.py
@@ -1,16 +1,18 @@
+import pytest
 from httpx import AsyncClient
 
 
-async def test_user_login_valid(client: AsyncClient) -> None:
-    response = await client.post(
-        "/api/access/login", data={"username": "superuser@example.com", "password": "Test_Password1!"}
-    )
-    assert response.status_code == 201
-    assert "access_token" in response.json()
-
-
-async def test_user_login_invalid(client: AsyncClient) -> None:
-    response = await client.post(
-        "/api/access/login", data={"username": "superuser@example1.com", "password": "Test_Password1!"}
-    )
-    assert response.status_code == 403
+@pytest.mark.parametrize(
+    "username,password,expected_status_code",
+    (
+        ("superuser@example1.com", "Test_Password1!", 403),
+        ("superuser@example.com", "Test_Password1!", 201),
+        ("user@example.com", "Test_Password1!", 403),
+        ("user@example.com", "Test_Password2!", 201),
+        ("inactive@example.com", "Old_Password2!", 403),
+        ("inactive@example.com", "Old_Password3!", 403),
+    ),
+)
+async def test_user_login(client: AsyncClient, username: str, password: str, expected_status_code: int) -> None:
+    response = await client.post("/api/access/login", data={"username": username, "password": password})
+    assert response.status_code == expected_status_code
