5d57a00bcddf7d3c986a2f852ad78cb9063e498d
0xSwego <alvinsartor@gmail.com>
2023-08-17 00:52:47 +0100

    fix: make sure sessions are closed in tests (#62)
    * fix: make sure sessions are closed in tests
    
    * fix: adjust spacing and function return type
    
    ---------
    
    Co-authored-by: 0xSwego <OxSwego@gmail.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from collections.abc import AsyncIterator, Generator
+from collections.abc import AsyncGenerator, AsyncIterator, Generator
 from pathlib import Path
 from typing import Any
 
@@ -79,8 +79,9 @@ def fx_session_maker_factory(engine: AsyncEngine) -> async_sessionmaker[AsyncSes
 
 
 @pytest.fixture(name="session")
-def fx_session(sessionmaker: async_sessionmaker[AsyncSession]) -> AsyncSession:
-    return sessionmaker()
+async def fx_session(sessionmaker: async_sessionmaker[AsyncSession]) -> AsyncGenerator[AsyncSession, None]:
+    async with sessionmaker() as session:
+        yield session
 
 
 @pytest.fixture(autouse=True)
