ad347c8c2fb3e9f5d603062d367178f2a5e89121
Cody Fincher <cody.fincher@gmail.com>
2023-08-19 13:50:54 -0500

    fix: updated config to align with current vue build

--- svelte.config.js
+++ svelte.config.js
@@ -1,34 +1,40 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/**
+ * @param {string} path
+ */
+function getBackendUrl(path) {
+	return `${process.env.FRONTEND_URL || 'http://localhost:8000'}${path}`;
+}
+const STATIC_URL = process.env.STATIC_URL || '/static/';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
-    files: {
-      assets: 'src/web/static',
-      lib: 'src/web/lib',
-      hooks: {
-        client: 'src/web/hooks/client',
-        server: 'src/web/hooks/server'
-      },
-      serviceWorker: 'src/web/service-worker',
-      appTemplate: 'src/web/app.html',
-      errorTemplate: 'src/web/error.html',
-      params: 'src/web/params',
-      routes: 'src/web/routes',
-    },
-    outDir: 'src/web/.svelte-kit',
-    
-
-  }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		files: {
+			assets: 'src/app/domain/web/resources/assets',
+			lib: 'src/app/domain/web/resources/lib',
+			hooks: {
+				client: 'src/app/domain/web/resources/hooks/client',
+				server: 'src/app/domain/web/resources/hooks/server'
+			},
+			serviceWorker: 'src/app/domain/web/resources/service-worker',
+			appTemplate: 'src/app/domain/web/resources/app.html',
+			errorTemplate: 'src/app/domain/web/resources/error.html',
+			params: 'src/app/domain/web/resources/params',
+			routes: 'src/app/domain/web/resources/routes'
+		},
+		outDir: 'src/app/domain/web/public/'
+	}
 };
 
 export default config;
