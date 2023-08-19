b6ff40f732b382ade52cc81786b1d0a3bf18e560
Cody Fincher <cody.fincher@gmail.com>
2023-08-19 14:09:20 -0500

    fix: svelte/vite config changes

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	base: `${STATIC_URL}`,
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,34 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
+import path from 'path';
+function getBackendUrl(path: string) {
+	return `${process.env.FRONTEND_URL || 'http://localhost:8000'}${path}`;
+}
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: ['.', path.join(__dirname, 'node_modules')]
+		},
+		host: '0.0.0.0',
+		port: 3005,
+		cors: true,
+		strictPort: true,
+		watch: {
+			ignored: ['node_modules', '.venv', '**/__pycache__/**']
+		},
+		proxy: {
+			'/api': {
+				target: getBackendUrl('/api'),
+				changeOrigin: true,
+				ws: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src/app/domain/web/resources')
+		}
+	}
 });
