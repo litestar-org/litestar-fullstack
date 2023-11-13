6399df486e99496332eb07c397d561f9556a10c2
Franz Forstmayr <forstmayr.franz@gmail.com>
2023-11-13 22:33:36 +0100

    Update health controller with right signature

--- src/app/domain/system/controllers.py
+++ src/app/domain/system/controllers.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, Literal, TypeVar
 
 from litestar import Controller, MediaType, get
 from litestar.response import Response
+from litestar_saq import TaskQueues
 from sqlalchemy import text
 
 from app.domain.system.dtos import SystemHealth
@@ -11,7 +12,6 @@ from app.lib import constants, log
 from app.lib.cache import redis
 
 if TYPE_CHECKING:
-    from litestar_saq import Queue
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -37,7 +37,7 @@ class SystemController(Controller):
         description="Execute a health check against backend components.  Returns system information including database and cache status.",
         signature_namespace={"SystemHealth": SystemHealth},
     )
-    async def check_system_health(self, db_session: AsyncSession, task_queues: list[Queue]) -> Response[SystemHealth]:
+    async def check_system_health(self, db_session: AsyncSession, task_queues: TaskQueues) -> Response[SystemHealth]:
         """Check database available and returns app config info."""
         try:
             await db_session.execute(text("select 1"))
@@ -48,7 +48,7 @@ class SystemController(Controller):
         db_status = "online" if db_ping else "offline"
         cache_ping = await redis.ping()
         cache_status = "online" if cache_ping else "offline"
-        worker_ping = bool([await queue.info() for queue in task_queues])
+        worker_ping = bool([await queue.info() for queue in task_queues.queues.values()])
         worker_status = "online" if worker_ping else "offline"
         healthy = bool(worker_ping and cache_ping and db_ping)
         if healthy:
