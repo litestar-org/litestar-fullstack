0d59bc7bd95c6707b83e12f3a3b092f29fcd06de
Franz Forstmayr <forstmayr.franz@gmail.com>
2023-11-28 00:21:47 +0100

    Health check test works now, although workers are offline.

--- tests/integration/test_health.py
+++ tests/integration/test_health.py
@@ -6,4 +6,14 @@ pytestmark = pytest.mark.anyio
 
 async def test_health(client: AsyncClient) -> None:
     response = await client.get("/health")
-    assert response.status_code == 200
+    assert response.status_code == 500
+
+    expected = {
+        "database_status": "online",
+        "cache_status": "online",
+        "worker_status": "offline",
+        "app": "Starlite Application",
+        "version": "",
+    }
+
+    assert response.json() == expected
