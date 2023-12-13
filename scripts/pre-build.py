fff246617ef96ddcb9618f388f5978d92203cfd9
Cody Fincher <cody.fincher@gmail.com>
2023-12-13 15:46:18 -0600

    feat: add node to nixpack; refactor `pre-build`

--- scripts/pre-build.py
+++ scripts/pre-build.py
@@ -8,7 +8,12 @@ import sys
 from pathlib import Path
 from typing import Any
 
-__all__ = ["build_npm_assets"]
+try:
+    import nodeenv  # type: ignore  # noqa: F401
+
+    NODEENV_INSTALLED = True
+except ImportError:
+    NODEENV_INSTALLED = False
 
 logger = logging.getLogger()
 
@@ -19,19 +24,22 @@ DEFAULT_VENV_PATH = Path(PROJECT_ROOT / ".venv")
 
 def build_npm_assets(setup_kwargs: Any) -> Any:
     # look for this in the environment and skip this function if it exists, sometimes building here is not needed, eg. when using nixpacks
-    if os.environ.get("LITESTAR_SKIP_BUILD_ASSETS") is not None:
-        logger.info("skipping npm asset building")
-        return setup_kwargs
-    found_in_local_venv = Path(DEFAULT_VENV_PATH / "bin" / NODEENV).exists()
-    nodeenv_command = f"{DEFAULT_VENV_PATH}/bin/{NODEENV}" if found_in_local_venv else NODEENV
-    install_dir = DEFAULT_VENV_PATH if found_in_local_venv else os.environ.get("VIRTUAL_ENV", sys.prefix)
+    no_nodeenv = os.environ.get("LITESTAR_SKIP_NODEENV_INSTALL") is not None or NODEENV_INSTALLED is False
     kwargs: dict[str, Any] = {}
+    if no_nodeenv:
+        logger.info("skipping nodeenv configuration")
+    else:
+        found_in_local_venv = Path(DEFAULT_VENV_PATH / "bin" / NODEENV).exists()
+        nodeenv_command = f"{DEFAULT_VENV_PATH}/bin/{NODEENV}" if found_in_local_venv else NODEENV
+        install_dir = DEFAULT_VENV_PATH if found_in_local_venv else os.environ.get("VIRTUAL_ENV", sys.prefix)
+        logger.info("Installing Node environment to %s:", install_dir)
+        subprocess.run([nodeenv_command, install_dir, "--force", "--quiet"], **kwargs)  # noqa: PLW1510
+
     if platform.system() == "Windows":
         kwargs["shell"] = True
-    logger.info("Installing Node environment to %s:", install_dir)
-    subprocess.run([nodeenv_command, install_dir, "--force", "--quiet"], **kwargs)  # noqa: PLW1510
-    subprocess.run(["npm", "ci", "install"], **kwargs)  # noqa: S607, PLW1510
-    logger.info("Building NPM assets:")
+    logger.info("Installing NPM packages.")
+    subprocess.run(["npm", "install"], **kwargs)  # noqa: S607, PLW1510
+    logger.info("Building NPM assets.")
     subprocess.run(["npm", "run", "build"], **kwargs)  # noqa: S607, PLW1510
     return setup_kwargs
 
