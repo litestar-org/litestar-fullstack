c8357514834a73323bbc46478c4d13bedf463cd8
Cody Fincher <204685+cofin@users.noreply.github.com>
2024-01-13 13:21:14 -0600

    fix: use `server_lifespan` (#123)
    * fix: upgrade packages and use server_lifespan
    
    * fix: type signature
    
    * feat: remove old test
    
    * fix: remove CLI from pyproject
    
    * fix: recreate lock file

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,6 +1,3 @@
-from typing import Any
-from unittest.mock import MagicMock
-
 import pytest
 from click.testing import CliRunner
 
@@ -8,12 +5,3 @@ from click.testing import CliRunner
 @pytest.fixture()
 def cli_runner() -> CliRunner:
     return CliRunner()
-
-
-def test_run_server(cli_runner: Any, monkeypatch: pytest.MonkeyPatch) -> None:
-    import app.cli
-
-    run_server_subprocess = MagicMock()
-    monkeypatch.setattr(app.cli, "run_all_app", run_server_subprocess)
-    result = cli_runner.invoke(app.cli.run_all_app)
-    assert result.exit_code == 0
