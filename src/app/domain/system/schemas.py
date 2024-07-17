9d15d33493b880dded07f5650f9917e5d1bb63df
Cody Fincher <cody.fincher@gmail.com>
2024-07-17 01:12:46 +0000

    feat: favicon

--- src/app/domain/system/schemas.py
+++ src/app/domain/system/schemas.py
@@ -1,16 +1,15 @@
-from dataclasses import dataclass
 from typing import Literal
 
 from app.__about__ import __version__ as current_version
 from app.config.base import get_settings
+from app.lib.schema import CamelizedBaseStruct
 
 __all__ = ("SystemHealth",)
 
 settings = get_settings()
 
 
-@dataclass
-class SystemHealth:
+class SystemHealth(CamelizedBaseStruct):
     database_status: Literal["online", "offline"]
     cache_status: Literal["online", "offline"]
     worker_status: Literal["online", "offline"]
