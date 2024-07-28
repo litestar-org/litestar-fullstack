39c904d3555a8583045d8b292ae8783a85b53083
Cody Fincher <cody.fincher@gmail.com>
2024-07-28 01:21:58 +0000

    chore: upgrade

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -15,7 +15,6 @@ from sqlalchemy.pool import NullPool
 
 from app.config import get_settings
 from app.db.models import Team, User
-from app.domain.accounts.guards import session_auth
 from app.domain.accounts.services import RoleService, UserService
 from app.domain.teams.services import TeamService
 from app.server.builder import ApplicationConfigurator
@@ -151,25 +150,3 @@ async def fx_client(app: Litestar) -> AsyncIterator[AsyncClient]:
     """
     async with AsyncClient(app=app, base_url="http://testserver", timeout=10) as client:
         yield client
-
-
-@pytest.fixture(name="superuser_token_headers")
-def fx_superuser_token_headers() -> dict[str, str]:
-    """Valid superuser token.
-
-    ```text
-    ValueError: The future belongs to a different loop than the one specified as the loop argument
-    ```
-    """
-    return {"Authorization": f"Bearer {session_auth.create_token(identifier='superuser@example.com')}"}
-
-
-@pytest.fixture(name="user_token_headers")
-def fx_user_token_headers() -> dict[str, str]:
-    """Valid user token.
-
-    ```text
-    ValueError: The future belongs to a different loop than the one specified as the loop argument
-    ```
-    """
-    return {"Authorization": f"Bearer {session_auth.create_token(identifier='user@example.com')}"}
