e643b1ccac4b3b3cea38798c6d975f322a435360
Cody Fincher <cody@litestar.dev>
2025-01-14 18:00:23 +0000

    fix: linting

--- src/app/db/models/team_tag.py
+++ src/app/db/models/team_tag.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from typing import Final
-
 from advanced_alchemy.base import orm_registry
 from sqlalchemy import Column, ForeignKey, Table
 
-team_tag: Final[Table] = Table(
+team_tag = Table(
     "team_tag",
     orm_registry.metadata,
     Column("team_id", ForeignKey("team.id", ondelete="CASCADE"), primary_key=True),
