82c393d39952d3c1dd1dbf23dc5e0de80740999f
Harshal Laheri <harshallaheri12@gmail.com>
2025-06-15 22:33:40 +0000

    fix: show login error

--- src/js/src/layouts/public-layout.tsx
+++ src/js/src/layouts/public-layout.tsx
@@ -1,8 +1,13 @@
+import { useTheme } from "@/lib/theme-context"
 import { Outlet } from "@tanstack/react-router"
+import { Toaster } from "sonner"
 
 export function PublicLayout() {
+  const { theme } = useTheme()
+
   return (
     <div className="flex min-h-screen flex-col">
+      <Toaster richColors theme={theme} position="top-right" />
       <main className="flex flex-1">
         <Outlet />
       </main>
